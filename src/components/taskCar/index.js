76b1a151885847e612f336176be3d739398eb50a
GabrielFelix23 <srgfelix123@gmail.com>
2021-02-08 21:51:52 -0300

    Listando carros

--- src/components/taskCar/index.js
+++ src/components/taskCar/index.js
@@ -4,16 +4,16 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import styles from './styles'
 import car from '../../assets/car.png'
 
-export default function TaskCar(){
+export default function TaskCar({model, price, km}){
     return(
         <View style={styles.Container}>
             <View style={styles.ContainerTask}>
                 <Image source={car} style={styles.car}/>
                 
                 <View style={styles.ContainerDescription}>
-                    <Text style={styles.TextDescription}>Carro: Sonata</Text>
-                    <Text style={styles.TextDescription}>R$ 50.000</Text>
-                    <Text style={styles.TextDescription}>KM/H 60.000</Text>
+                    <Text style={styles.TextDescription}>Carro: {model}</Text>
+                    <Text style={styles.TextDescription}>R$ {price}</Text>
+                    <Text style={styles.TextDescription}>KM/H {km}</Text>
                 </View>
 
                 <TouchableOpacity style={styles.button}>
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { View, ScrollView } from 'react-native'
 
 import styles from './styles'
@@ -7,14 +7,30 @@ import Header from '../../components/header'
 import Footer from '../../components/footer'
 import TaskCar from '../../components/taskCar'
 
+import api from '../../services/api'
+
 export default function Home() {
+  const [list, setList] = useState([])
+
+  async function CarList(){
+    await api.get('/filter/all/11:11:11:11:11:11')
+    .then((response) => {
+      setList(response.data)
+    })
+  }
+
+  useEffect(() => {
+    CarList()
+  }, [])
   return (
     <View style={styles.Container}>
       <Header task={'add'}/>
 
       <ScrollView>
         <View style={styles.ContainerTaskCar}>
-          <TaskCar/>
+          {list.map((l) => (
+            <TaskCar model={l.model} price={l.price} km={l.km}/>
+          ))}
         </View>
       </ScrollView>
 
