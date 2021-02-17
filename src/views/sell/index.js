24e19ab9a5020a4e93b275fcf6ac2d29212ca887
GabrielFelix23 <srgfelix123@gmail.com>
2021-02-16 23:35:30 -0300

    Inserindo Inputs

--- src/views/sell/index.js
+++ src/views/sell/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TextInput, KeyboardAvoidingView, ScrollView} from 'react-native'
 
 //Components
 import Header from '../../components/header'
@@ -9,10 +9,46 @@ import styles from './styles'
 
 export default function Sell({navigation}){
     return(
-        <View style={styles.container}>
+        <KeyboardAvoidingView style={styles.container}>
             <Header task={'back'} navigation={navigation}/>
-            <Text>Sell</Text>
+                <ScrollView contentContainerStyle={styles.containerInputs}>
+                    <Text>Marca</Text>
+                    <TextInput placeholder="Marca..."/>
+
+                    <Text>Modelo</Text>
+                    <TextInput placeholder="Modelo..."/>
+
+                    <Text>Preço</Text>
+                    <TextInput placeholder="Preço..."/>
+
+                    <Text>Chassi</Text>
+                    <TextInput placeholder="Chassi..."/>
+
+                    <Text>Ano</Text>
+                    <TextInput placeholder="Ano..."/>
+
+                    <Text>KM/H</Text>
+                    <TextInput placeholder="KM/H..."/>
+
+                    <Text>Câmbio</Text>
+                    <TextInput placeholder="Câmbio..."/>
+
+                    <Text>Portas</Text>
+                    <TextInput placeholder="Portas..."/>
+
+                    <Text>Cor</Text>
+                    <TextInput placeholder="Cor..."/>
+
+                    <Text>Blindado</Text>
+                    <TextInput placeholder="Blindado..."/>
+
+                    <Text>Estado</Text>
+                    <TextInput placeholder="Estado..."/>
+
+                    <Text>Cidade</Text>
+                    <TextInput placeholder="Cidade..."/>
+                </ScrollView> 
             <Footer task={'save'}/>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
