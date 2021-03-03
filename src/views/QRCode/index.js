28df940187cc3b4f310507bab57eac483bc21f3c
GabrielFelix23 <srgfelix123@gmail.com>
2021-03-03 16:03:20 -0300

    QRCODE finalizado.

--- src/views/QRCode/index.js
+++ src/views/QRCode/index.js
@@ -1,17 +1,60 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, Text, Alert, StyleSheet, TouchableOpacity} from 'react-native'
+import {BarCodeScanner} from 'expo-barcode-scanner'
+import * as Network from 'expo-network'
 
 import styles from './styles'
 
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 
-export default function QRCode(){
+export default function QRCode({navigation}){
+    const [hasPermission, setHasPermission] = useState(null)
+    const [scanned, setScanned] = useState(false)
+
+    async function getMacAddress(){
+        await Network.getMacAddressAsync().then(mac => {
+            Alert.alert(mac)
+        })
+    }
+
+    const handleBarCodeScanned = ({data}) => {
+        setScanned(true);
+        if(data == 'getMacaddress')
+            getMacAddress()
+        else
+            Alert.alert('QRCODE inválido!')
+    }
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync()
+            setHasPermission(status === 'granted');
+          })()
+    },[])
     return(
         <View style={styles.container}>
-            <Header/>
-            <Text>QRCODE</Text>
-            <Footer/>
+            <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={StyleSheet.absoluteFillObject}/>
+
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Conectar com minha conta na web</Text>
+            </View>
+
+            <View style={styles.contianerButtons}>
+                <View style={styles.buttons}>
+                    <TouchableOpacity 
+                    style={styles.buttonBack}
+                    onPress={() => navigation.navigate('Home')}>
+                        <Text style={styles.textButton}>Voltar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                    style={scanned ? styles.buttonScan : styles.buttonScanInative}
+                    onPress={() => setScanned(false)}>
+                        <Text style={styles.textButton}>Scan Novamente</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
--- src/views/QRCode/styles.js
+++ src/views/QRCode/styles.js
@@ -3,6 +3,60 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
     container:{
         flex: 1
+    },
+    header:{
+        width: '100%',
+        height: 70,
+        backgroundColor: '#F53131',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomColor: '#CC0000',
+        borderBottomWidth: 5
+    },
+    headerText:{
+        color: "#fff",
+        fontSize: 20
+    },
+    contianerButtons:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttons:{
+        flexDirection: 'row',
+        position: 'relative',
+        top: '120%'
+    },
+    buttonBack:{
+        backgroundColor: '#F53131',
+        width: 200,
+        height: 50,
+        borderTopLeftRadius: 10,
+
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textButton:{
+        fontSize: 22,
+        color: "#fff"
+    },
+    buttonScan:{
+        backgroundColor: '#0080FF',
+        width: 200,
+        height: 50,
+        borderBottomRightRadius: 10,
+
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonScanInative:{
+        backgroundColor: '#0080FF',
+        width: 200,
+        height: 50,
+        borderBottomRightRadius: 10,
+        opacity: 0.5,
+
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
